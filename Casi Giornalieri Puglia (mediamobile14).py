8bcf7909279d5c1404df863349812ae466663b34
Giuseppe Maiorano <40656151+giuseppemaiorano@users.noreply.github.com>
2021-01-21 00:55:14 +0100

    Add files via upload

---
+++ Casi Giornalieri Puglia (mediamobile14).py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+df = pd.read_csv("Coronavirus Italia_Main Dashboard_Time series (62).csv")
+df.head(10)
+
+
+# In[7]:
+
+
+plt.figure(figsize=[15,10])
+plt.grid(True)
+plt.plot(df['Daily Cases'], label='Casi Giornalieri', linewidth=2, linestyle='--')
+plt.xlabel('Giorni')
+plt.ylabel('Casi Giornalieri')
+plt.title("Casi Giornalieri Puglia", fontsize=20)
+plt.legend(loc=2)
+
+
+# In[8]:
+
+
+for i in range(0,df.shape[0]-13):
+    df.loc[df.index[i+13],'SMA_14'] = np.round(((df.iloc[i,1]+ df.iloc[i+1,1] +df.iloc[i+2,1]+df.iloc[i+3,1]+ df.iloc[i+4,1]+ df.iloc[i+5,1]+ df.iloc[i+6,1]+ df.iloc[i+7,1]+ df.iloc[i+8,1]+ df.iloc[i+9,1]+ df.iloc[i+10,1]+ df.iloc[i+11,1]+ df.iloc[i+12,1]+ df.iloc[i+13,1])/14),1)
+
+
+# In[9]:
+
+
+df['pandas_SMA_14'] = df.iloc[:,1].rolling(window=14).mean()
+
+
+# In[15]:
+
+
+plt.figure(figsize=[15,10])
+plt.grid(True)
+plt.plot(df['Daily Cases'],label='Casi Giornalieri', linewidth=2, linestyle='--', color='red')
+plt.plot(df['SMA_14'],label='Media Mobile 14 giorni', linewidth=8)
+plt.xlabel('Data')
+plt.ylabel('Casi Giornalieri')
+plt.title("Andamento Casi Puglia (Seconda Ondata)", fontsize=20)
+plt.legend(loc=2)
+
+
+# In[ ]:
+
+
+
+
